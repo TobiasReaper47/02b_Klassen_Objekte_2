9edc9cc5a0fb28931be88e34bda335df5fd80932
TobiasReaper47 <tobiasreaper273@gmail.com>
2025-01-17 10:40:23 +0100

    Objekte/Eigenschaften zuweisen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,14 +5,33 @@ public class App {
     public static void main(String[] args) {
 
         Cat cat = new Cat();
-        output("Blick von aussen: " + cat);
-        output("Blick von innen: " + cat.getInstanceVariable());
+        // output("Blick von aussen: " + cat);
+        // output("Blick von innen: " + cat.getInstanceVariable());
+
+        // Werte zuweisen
+        cat.firstName = "Grizzabella";
+        cat.furColor = "white";
+        cat.age = 29;
+
+        // Werte ausgeben
+        output(cat.firstName);
+        output(cat.furColor);
+        output(Integer.toString(cat.age));  // EXPLIZITE Typkonvertierung
 
         output("-----------------------");
 
         Cat cat2 = new Cat();
-        output("Blick von aussen: " + cat2);
-        output("Blick von innen: " + cat2.getInstanceVariable());
+        // output("Blick von aussen: " + cat2);
+        // output("Blick von innen: " + cat2.getInstanceVariable());
+
+        cat2.firstName = "Alonzo";
+        cat2.furColor = "grey";
+        cat2.age = 58;
+
+        // Werte ausgeben
+        output(cat2.firstName);
+        output(cat2.furColor);
+        output(Integer.toString(cat2.age));
 
     }
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,6 +2,14 @@ package com.cc.java;
 
 public class Cat {
 
+    public String firstName;
+    public String furColor;
+    public int age;
+
+
+
+
+
 
     public Cat getInstanceVariable(){
        return this;
